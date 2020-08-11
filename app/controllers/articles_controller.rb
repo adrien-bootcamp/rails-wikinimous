898822bfa21a6de93b951025d309ff84aaf08c6f
Adrien Fort <adrien.fort01@gmail.com>
2020-08-11 14:54:00 +0200

    add edit and update in the controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,21 +9,26 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
   def create
-    @article = Article.new(artcile_params)
+    @article = Article.new(article_params)
     @article.save
 
     redirect_to articles_path
   end
   def edit
+    @article = Article.find(params[:id])
   end
   def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+
+    redirect_to articles_path
   end
   def destroy
   end
 
   private
 
-  def artcile_params
+  def article_params
     params.require(:article).permit(:title, :content)
   end
 end
